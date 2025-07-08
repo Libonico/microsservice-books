127f053b05a2e5067ea1f45ffe3edb41988ab9fd
Libonico <pedrorochaliboni@hotmail.com>
2025-07-08 15:45:15 -0300

    Add files via upload

--- src/main/java/com/example/Books/microsservice_Books_main/service/BookService.java
+++ src/main/java/com/example/Books/microsservice_Books_main/service/BookService.java
@@ -2,12 +2,10 @@ package com.example.Books.microsservice_Books_main.service;
 
 import com.example.Books.microsservice_Books_main.entity.Book;
 import com.example.Books.microsservice_Books_main.exception.ResourceNotFoundException;
-import com.example.Books.microsservice_Books_main.enums.ContentRatingEnum;
 import com.example.Books.microsservice_Books_main.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -15,18 +13,20 @@ import java.util.logging.Logger;
 public class BookService {
 
     public final AtomicLong counter = new AtomicLong();
-    private Logger logger = Logger.getLogger(BookService.class.getName());
+    private Logger logger = Logger.getLogger(BookService.class.getName()); //cria o logger
 
     @Autowired
     private BookRepository bookRepository;
 
+    //função para registro de livros
     public Book register(Book book) {
-        logger.info("Book registred.");
+        logger.info("Book registred."); //logger para fins de confirmação através do console
         return bookRepository.save(book);
     }
 
+    //função para edição de livros
     public Book edit(Book book) {
-        logger.info("The informations has been edited.");
+        logger.info("The informations has been edited."); //logger para fins de confirmação através do console
         Book entity = bookRepository.findById(book.getId()).
                 orElseThrow(() -> new ResourceNotFoundException("Book not found!"));
 
@@ -38,40 +38,24 @@ public class BookService {
         return bookRepository.save(book);
     }
 
+    //função para remoção de livros do sistema
     public void remove(Long id) {
-        logger.info("Book removed.");
+        logger.info("Book removed."); //logger para fins de confirmação através do console
         Book entity = bookRepository.findById(id).
                 orElseThrow(() -> new ResourceNotFoundException("Book not found!"));
 
-        bookRepository.deleteById(id);
+        bookRepository.delete(entity);
     }
 
-    public Book searchBook(Long id){
-        logger.info("Book found.");
+    //função de localização do livro pelo id
+    public Book findBookById(Long id){
+        logger.info("Book found."); //logger para fins de confirmação através do console
         return bookRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Book not found!"));
     }
 
-    public List<Book> listBooks() {
-        logger.info("Books found.");
+    //função de localização de todos os livros cadastrados
+    public List<Book> listAllBooks() {
+        logger.info("Books found."); //logger para fins de confirmação através do console
         return bookRepository.findAll();
-    }
-
-    public List<Book> listBooksByContentRating(String contentRating) {
-        logger.info("Books found.");
-        if(contentRating != null && !contentRating.isEmpty()) {
-            ContentRatingEnum contentRatingEnum = ContentRatingEnum.valueOf(contentRating.toUpperCase());
-            return bookRepository.findBooksByContentRating(contentRatingEnum);
-        } else {
-            return bookRepository.findAll();
-        }
-    }
-
-    public List<Book> listBooksByTitleContainingIgnoreCase(String title) {
-        logger.info("Books found.");
-        if(title != null && !title.trim().isEmpty()) {
-            return bookRepository.findBooksByTitleContainingIgnoreCase(title);
-        } else {
-            return bookRepository.findAll();
-        }
     }
 }
