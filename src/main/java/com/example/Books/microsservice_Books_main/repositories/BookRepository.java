edc950b3702e8f002d614c994bc91fe8f9545d48
Graciele Miki Sazaka <gracielesazaka@gmail.com>
2025-07-09 09:07:54 -0300

    Add files via upload

--- src/main/java/com/example/Books/microsservice_Books_main/repositories/BookRepository.java
+++ src/main/java/com/example/Books/microsservice_Books_main/repositories/BookRepository.java
@@ -1,8 +1,14 @@
 package com.example.Books.microsservice_Books_main.repositories;
 
 import com.example.Books.microsservice_Books_main.entity.Book;
+import com.example.Books.microsservice_Books_main.enums.ContentRatingEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long> {}
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findBooksByContentRating(ContentRatingEnum contentRating);
+    List<Book> findBooksByTitleContainingIgnoreCase(String title);
+}
